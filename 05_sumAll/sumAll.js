e0c0b9bca8da818e70029afb5c0432a67e43864f
Derek <cklove2211@gmail.com>
2024-05-27 06:30:04 +0700

    Updated

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start < 0 || end < 0) return 'ERROR';
+    if (typeof(arguments[1]) != typeof(arguments[0])) return 'ERROR';
+    if (start > end) [start,end] = [end,start];
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
