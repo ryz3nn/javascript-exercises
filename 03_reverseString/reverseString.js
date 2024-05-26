12fd20cc3ce80fa0c70e0c0e3a8af554c6cba317
Derek <cklove2211@gmail.com>
2024-05-26 17:27:34 +0700

    Updated

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // Classic version
+    // let wlen = word.length;
+    // const reverseText = [];
+    // for (let i = 0, j = wlen - 1; i <=  wlen && j >= 0; i++, j--) {
+    //     reverseText[i] = word.charAt(j);
+    // }
+    // return reverseText.join('');
 
+    // Modern version
+    return word.split('').reverse().join('');
 };
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
