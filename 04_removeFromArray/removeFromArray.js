f052b58ba8106ab14a5fd2fbc9f67847f782496c
Derek <cklove2211@gmail.com>
2024-05-26 18:48:09 +0700

    Updated

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeList) {
+    // First method
+    // for (let i = 0; i <= Arr.length - 1; i++) {
+    //     for (item of removeList) {
+    //         if (Arr[i] === item) {
+    //             Arr.splice(i,1);
+    //             i--;
+    //             break;
+    //         }
+    //     }
+    // }
+    // return Arr;
+
+    // Secod method
+    const newArray = [];
+    array.forEach((item) => {
+        if(!removeList.includes(item))
+            newArray.push(item);
+    });
+    return newArray;
 };
+removeFromArray([1,2,3], '1',3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
