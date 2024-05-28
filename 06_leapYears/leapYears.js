faf124564a9d44dadb7bf24c4fec25413d84a83d
Derek <cklove2211@gmail.com>
2024-05-28 08:21:28 +0700

    Updated

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const { yargsOptions } = require("jest-cli");
 
+const leapYears = function(year) {
+    // if (year % 400 == 0) return true;
+    // else if (year % 4 == 0 && year % 100 != 0) return true;
+    // else return false;
+    // Modern version
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 };
 
 // Do not edit below this line
