49c77d882f89d2af6146ac399f6ed7f2a24c4743
Derek <cklove2211@gmail.com>
2024-05-28 15:02:01 +0700

    Updated

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multi = 1;
+  for (let i = 0; i < array.length; i++) {
+    multi *= array[i];
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let pow = 1;
+	for (let i = 0; i < exponent; i++) {
+    pow *= num;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  let sum = 0;
+  if (number === 0 || number === 1) return 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
