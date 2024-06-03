a81a4c2c9082fc837f5ee6f1159ffe8ba7407c98
Derek <cklove2211@gmail.com>
2024-06-03 11:30:12 +0700

    Updated

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookLib) {
+  const books = Array.from(bookLib);
+  return books.map((book) => book.title)
 };
 
 // Do not edit below this line
