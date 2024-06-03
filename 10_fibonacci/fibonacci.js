761dac52f1cb79530b2adbca11dab15e643e1748
Derek <cklove2211@gmail.com>
2024-06-03 11:18:57 +0700

    Updated

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(ordinal) { 
+  const base = [1,1];
+  if (ordinal == 0) return 0;
+  if (ordinal == 1) return 1;
+  if (ordinal == 2) return 1;
+  if (ordinal >= 2) {
+    for (let i = 2; i < ordinal; i++) {
+      base.push(base[i-2] + base[i-1]);
+    }
+    return base[ordinal - 1];
+  }
+  else return "OOPS"
 };
 
 // Do not edit below this line
